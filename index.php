d601b6f933033927ae775a274edd26016f20c683
Zoex2304 <zikrifadhiilahahmad@gmail.com>
2025-07-06 10:28:45 +0700

    first up

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <title>Presensi Mahasiswa</title>
+  <style>
+    body, html {
+      margin: 0;
+      padding: 0;
+      height: 100%;
+    }
+    iframe {
+      width: 100%;
+      height: 100vh;
+      border: none;
+    }
+  </style>
+</head>
+<body>
+  <!-- Ganti URL ngrok kamu di bawah -->
+  <iframe src="https://269a-103-3-222-34.ngrok-free.app"></iframe>
+</body>
+</html>
